e880d0cebab389c41781c2621eeb7f8af30662db
unknown <victoria.clark@44385-WIN-501.auckland.natcoll.ac.nz>
2014-08-15 11:48:25 +1200

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// $("h2").click(function(){
+// 	$("h2").fadeOut("slow");
+// });
+
+// $(".headers").click(function(){
+// 	var oHeaders = $(".headers").fadeOut("slow");
+// 	oHeaders = $(".headers").fadeIn("slow");
+// });
+
+// $("img").click(function(){
+
+// 	var oImg = $("img").fadeOut("slow");
+// 	oImg = $("img").fadeIn("slow");
+
+// });
+
+$("button").click(function(){
+
+	$("#nav-list").slideToggle("slow");
+
+
+});
